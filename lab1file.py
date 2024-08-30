ba0bccd084d1b47c8a4d56a1109b60fecd397ba0
kBunn411 <117461180+kBunn411@users.noreply.github.com>
2024-08-30 12:48:26 -0400

    Update lab1file.py

--- lab1file.py
+++ lab1file.py
@@ -2,30 +2,68 @@ import math
 I = 7
 R = 1.2
 D = int(input("Enter the amount of Days that the infection has occured: "))
+
 x = 0
 tui = 0
-sum = I
+
+
+
+
+
+
+sum = 0
+
+sum2 = I
+    
 
 for i in range(D):
-    sum = R * sum
-    sum = math.trunc(sum)
+    
+    sum = I * R
+    sum2 = sum2 + sum
+    I = sum
+
+
+    
+
     if (i == 14):
         tui = 9972 * sum
         x=1
+
+
+
+sum2 = math.trunc(sum2)   
 if x == 1:
-    print (sum)
+    math
+    print (sum2)
     print (tui)
+
 else:
-    print (sum)
-    print (sum * 9972)
+    print (sum2)
+    print (sum2 * 9972)
 
 S = int(input("Press 1 to continue or any other number to stop again"))
 
+
 while(S == 1):
-    D = int(input("Enter the amount of Days that the infection has occured"))
     sum = I
+
+    D = int(input("Enter the amount of Days that the infection has occured"))
+
     for i in range(D):
+        
         sum = R * sum
         sum = math.trunc(sum)
-    print (sum)
+
+        if (i == 14):
+            tui = 9972 * sum
+            x=1
+    
+    if x == 1:
+        print (sum)
+        print (tui)
+
+    else:
+        print (sum)
+        print (sum * 9972)
+    
     S = int(input("Press 1 to continue or any other number to stop again"))
